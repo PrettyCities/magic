1a4f99e9c47385da6c5c5650144700211639fec3
prettycities <mrcyze@gmail.com>
2018-04-02 20:23:01 -0500

    -m

---
+++ image-recognition/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Image Rocognition',
+      version='1.0',
+      description='Tools for recognizing and processing images.',
+      author='David Cyze',
+      author_email='mrcyze@gmail.com',
+      packages=['imagerecognition'],
+      )
